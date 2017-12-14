44f4e4e79c8f7ebf63135601157d966a907d975e
anbe6083 <anbe6083@colorado.edu>
2017-12-13 20:30:50 -0800

    Added 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reverseString(String s) {
+        if(s == null)
+            return null;
+        StringBuilder result = new StringBuilder();
+        for(int i = s.length() - 1; i >= 0; i--) {
+            result.append(s.charAt(i));
+        }
+        return result.toString();
+    }
+}
