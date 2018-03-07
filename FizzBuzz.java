bd1e2cae3a229c0547c2c3f21ad92d411651dd3a
anbe6083 <anbe6083@colorado.edu>
2018-03-07 13:35:42 -0800

    Updated fizzbuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<String>();
+        List<String> result = new ArrayList();
         for(int i = 1; i <= n; i++) {
-            if(i < 3) {
-                result.add(Integer.toString(i));
-            } else {
-                if(i % 3 == 0 && i % 5 == 0)
-                    result.add("FizzBuzz");
-                else if(i % 3 == 0) 
-                    result.add("Fizz");
-                else if(i % 5 == 0) 
-                    result.add("Buzz");
-                else 
-                    result.add(Integer.toString(i));
-            }
+            String output = "";
+            if(i % 3 == 0)
+                output += "Fizz";
+            if(i % 5 == 0)
+                output += "Buzz";
+            if(i % 3 != 0 && i % 5 != 0)
+                output = Integer.toString(i);
+            result.add(output);
         }
         return result;
     }
